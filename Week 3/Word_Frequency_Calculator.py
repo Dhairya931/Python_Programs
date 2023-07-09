9be6688afdec51a09bd84be33adb7db1eb85923d
Dhairya931 <dhairyamaheshwari222@gmail.com>
2023-07-09 09:42:34 +0530

    Version 1

---
+++ Week 3/Word_Frequency_Calculator.py
@@ -0,0 +1,20 @@
+s=input("Enter a sentence:")
+dic={}
+lst=s.split()
+
+for i in lst:
+    if i[-1]==".":
+        i=i[0:len(i)-1]
+        lst.append(i)
+        
+    elif i in dic:
+        dic[i]+=1
+
+    else:
+        dic.update({i:1})  
+
+for j in dic:
+    print ("Frequency of ", j, end = " ")
+    print (":", end = " ")
+    print (dic[j], end = " ")
+    print()
