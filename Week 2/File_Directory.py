8fd638230e51713e571c7ad3d9b0d04ec1446d22
Dhairya931 <dhairyamaheshwari222@gmail.com>
2023-07-09 10:21:56 +0530

    File_Directory.py

--- Week 2/File_Directory.py
+++ Week 2/File_Directory.py
@@ -9,22 +9,17 @@ for i in li:
     fileName, ext = os.path.splitext(i)
 
     ext = ext[1:]
-<<<<<<< HEAD
-    
     if ext== "":
         continue
-=======
+
     
     
->>>>>>> b314723b9f8325eede3f443859808951c1839447
+
         
     if os.path.exists( a + '/' + ext ):
         shutil.move( a + '/' + i, a + '/' + ext + '/' + i )
         
     else:
         os.makedirs( a + '/' + ext )
-<<<<<<< HEAD
         shutil.move( a + '/' + i, a + '/' + ext + '/' + i )
-=======
         shutil.move( a + '/' + i, a + '/' + ext + '/' + i )
->>>>>>> b314723b9f8325eede3f443859808951c1839447
