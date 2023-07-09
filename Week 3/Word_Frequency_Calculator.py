4f563c6fe02e16377abd249647623e99120a24da
Dhairya931 <dhairyamaheshwari222@gmail.com>
2023-07-09 09:53:16 +0530

    Version 1

--- Week 3/Word_Frequency_Calculator.py
+++ Week 3/Word_Frequency_Calculator.py
@@ -6,7 +6,7 @@ for i in lst:
     if i[-1]==".":
         i=i[0:len(i)-1]
         lst.append(i)
-        
+
     elif i in dic:
         dic[i]+=1
 
@@ -17,4 +17,4 @@ for j in dic:
     print ("Frequency of ", j, end = " ")
     print (":", end = " ")
     print (dic[j], end = " ")
-    print()
+    
